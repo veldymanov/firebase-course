dfa9421c459599b91f328fb3a9f14ee1d870388c
aveldymanov <aveld@softserveinc.com>
2020-02-13 13:43:41 +0200

    ng add @angular/fire@next

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,13 @@
 export const environment = {
   production: false,
   firebase: {
-
+    apiKey: 'AIzaSyAXxlqxYXySVMga4g3Qie0faAA0BKrIRVM',
+    authDomain: 'fir-course-f6088.firebaseapp.com',
+    databaseURL: 'https://fir-course-f6088.firebaseio.com',
+    projectId: 'fir-course-f6088',
+    storageBucket: 'fir-course-f6088.appspot.com',
+    messagingSenderId: '339808080680',
+    appId: '1:339808080680:web:29f49c01e9eee04889b61d',
+    measurementId: 'G-WDJSDY2EM5'
   }
 };
