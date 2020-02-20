44e98fcb20436e360bb16a394662f9a4ccd60a8d
Veldymanov <veldymanov@gmail.com>
2020-02-20 14:12:17 +0200

    batched writes & transactions implemented

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { of } from 'rxjs';
+import { FirestoreUtilsService } from '../services/firestore-utils.service';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'about',
@@ -7,10 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private db: AngularFirestore,
+    private fsUtils: FirestoreUtilsService,
+  ) { }
 
-  ngOnInit() {
-
+  ngOnInit(): void {
+    const courseRef = this.db.doc('/courses/3RtttYVC1VvTVYoBge2r')
+      .snapshotChanges()
+      .subscribe(snap => {
+        const course = snap.payload.data();
+        console.log(course);
+      });
   }
 
+  public save(): void {
+    const firebaseCourseRef = this.db.doc('/courses/1qmL1O7wWMRz05KVHSOh').ref;
+    const rxjsCourseRef = this.db.doc('/courses/3RtttYVC1VvTVYoBge2r').ref;
+    const batch = this.db.firestore.batch();
+
+    batch.update(firebaseCourseRef, {titles: {description: 'firebase course 28'}});
+    batch.update(rxjsCourseRef, {titles: {description: 'rxjs course 28'}});
+
+    const batch$ = of(batch.commit());
+  }
+
+  public async runTransaction(): Promise<number> {
+    const newCounter = await this.db.firestore.runTransaction(async (transaction) => {
+      console.log('Running transaction ...');
+
+      const courseRef = this.db.doc('/courses/1qmL1O7wWMRz05KVHSOh').ref;
+      const snap = await transaction.get(courseRef);
+      const course = <Course>snap.data();
+      const lessonsCount = course.lessonsCount + 1;
+
+      transaction.update(courseRef, {lessonsCount});
+
+      // use in the app only this returned value!!!
+      // because it is possible that transaction can be stopped
+      // and then re-run again
+      return lessonsCount;
+    });
+
+    console.log('newCounter', newCounter);
+
+    return newCounter;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { MatTabsModule } from '@angular/material/tabs';
-import { CoursesCardListComponent } from './courses-card-list/courses-card-list.component';
-import { CourseComponent } from './course/course.component';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
@@ -25,17 +20,26 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { CourseResolver } from './services/course.resolver';
-import { CourseDialogComponent } from './course-dialog/course-dialog.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatMomentDateModule } from '@angular/material-moment-adapter';
-import { AngularFireModule } from '@angular/fire';
 
-import { environment } from '../environments/environment';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+
+import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CourseResolver } from './services/course.resolver';
+import { CoursesCardListComponent } from './courses-card-list/courses-card-list.component';
+import { CourseComponent } from './course/course.component';
+import { CourseDialogComponent } from './course-dialog/course-dialog.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
+
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
@@ -72,7 +76,7 @@ import { LoginComponent } from './login/login.component';
     ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
-    AngularFirestoreModule,
+    AngularFirestoreModule, //.enablePersistence(),
     AngularFireStorageModule
   ],
   providers: [
