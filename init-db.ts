8a79e5797d2106fe09fba492980fbaf030399d12
Veldymanov <veldymanov@gmail.com>
2020-02-23 20:55:09 +0200

    test deployment to staging

--- init-db.ts
+++ init-db.ts
@@ -4,14 +4,14 @@ import { COURSES, findLessonsForCourse } from './db-data';
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyAXxlqxYXySVMga4g3Qie0faAA0BKrIRVM',
-  authDomain: 'fir-course-f6088.firebaseapp.com',
-  databaseURL: 'https://fir-course-f6088.firebaseio.com',
-  projectId: 'fir-course-f6088',
-  storageBucket: 'fir-course-f6088.appspot.com',
-  messagingSenderId: '339808080680',
-  appId: '1:339808080680:web:29f49c01e9eee04889b61d',
-  measurementId: 'G-WDJSDY2EM5'
+  apiKey: "AIzaSyA39gJEZJk2taPuAK-xxiBlnwScr58As-c",
+  authDomain: "fir-course-staging.firebaseapp.com",
+  databaseURL: "https://fir-course-staging.firebaseio.com",
+  projectId: "fir-course-staging",
+  storageBucket: "fir-course-staging.appspot.com",
+  messagingSenderId: "983755097736",
+  appId: "1:983755097736:web:b06115e2c977ff5359413d",
+  measurementId: "G-CC3P96XK5G"
 };
 
 console.log('Uploading data to the database with the following config:');
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA39gJEZJk2taPuAK-xxiBlnwScr58As-c",
+    authDomain: "fir-course-staging.firebaseapp.com",
+    databaseURL: "https://fir-course-staging.firebaseio.com",
+    projectId: "fir-course-staging",
+    storageBucket: "fir-course-staging.appspot.com",
+    messagingSenderId: "983755097736",
+    appId: "1:983755097736:web:b06115e2c977ff5359413d",
+    measurementId: "G-CC3P96XK5G"
+  }
+};
